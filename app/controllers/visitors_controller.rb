3eb65fa30cd0996c2db0539288686bdd89dac3fe
ggoral <goral.gonzalo@gmail.com>
2014-03-31 16:45:41 -0300

    add visitor controller

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,8 +1,12 @@
 class VisitorsController < ApplicationController
 
   def new
+    Rails.logger.debug 'DEBUG: entering new method'
     @owner = Owner.new
-    render 'visitors/new'
+    Rails.logger.debug 'DEBUG: Owner name is ' + @owner.name
+    #render 'visitors/new'
+    DISASTER
+
   end
 
 end
