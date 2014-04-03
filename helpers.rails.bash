58d7eaa1ea461cfc8074c100af3ea33cc3c49363
ggoral <goral.gonzalo@gmail.com>
2014-04-03 10:55:17 -0300

    add rails tips

---
+++ helpers.rails.bash
@@ -0,0 +1,9 @@
+# Start rails server
+bundle exec rails server
+
+# Generate a model
+bundle exec rails generate model
+
+# Create model, view, and controller
+bundle exec rails generate scaffold 
+
