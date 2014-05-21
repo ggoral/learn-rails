f4252d831450a4825635cd19af0a6d86808ae8ad
ggoral <goral.gonzalo@gmail.com>
2014-05-21 00:55:39 -0300

    add 'about' and 'contact' pages

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,11 +1,16 @@
 class VisitorsController < ApplicationController
 
   def new
-    Rails.logger.debug 'DEBUG: entering new method'
-    @owner = Owner.new
-    Rails.logger.debug 'DEBUG: Owner name is ' + @owner.name
+    #Rails.logger.debug 'DEBUG: entering new method'
+    #@owner = Owner.new
+    #Rails.logger.debug 'DEBUG: Owner name is ' + @owner.name
     #render 'visitors/new'
-    DISASTER
+    #render 'visitors/new', :layout => false
+    #DISASTER
+    #raise 'Deliberate Failure'
+    @owner = Owner.new
+    flash[:notice] = 'Welcome!'
+    flash[:alert] = 'My birthday is soon.'
   end
 
 end
